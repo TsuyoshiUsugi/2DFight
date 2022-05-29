8075f6dcae1de34f2c3e104b405a948824c886fd
ap/gh <qesulive@gmail.com>
2022-05-29 12:02:52 +0900

    ロゴと背景変更

---
+++ Assets/Scripts/kakuteiScripts/moveScene/Quit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Quit : MonoBehaviour
+{
+    public void OncliclkStartButton()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+
+        Application.Quit();
+    }
+}
