4221bcbd25a0de0eeb3159c7d2f5340f7324b56e
TsuyosiUsugi <qesulive@gmail.com>
2022-06-27 22:11:55 +0900

    arrowgenerator訂正中

--- Assets/Scripts/kakuteiScripts/BattleMode/ArrowGenerator.cs
+++ Assets/Scripts/kakuteiScripts/BattleMode/ArrowGenerator.cs
@@ -7,20 +7,30 @@ public class ArrowGenerator : MonoBehaviour
 
     GameObject _player1;
     Rigidbody2D _rb;
-    // Start is called before the first frame update
+
+    /// <summary>Arrowのダメージ/// </summary>
+    [SerializeField] float damage = 10;
+    /// <summary>Arrowの右方向の速度/// </summary>
+    [SerializeField] Vector2 rightVelocity = new Vector2(10, 0);
+    /// <summary>Arrowの左方向の速度/// </summary>
+    [SerializeField] Vector2 leftVelocity = new Vector2(-10, 0);
+
+    /// <summary> 右から当たった時の衝撃/// </summary>
+    [SerializeField] int rightImpactPower = 1000; 
     void Start()
     {
-        player1 = GameObject.FindGameObjectWithTag("Player1");
-        rb = GetComponent<Rigidbody2D>();
+        _player1 = GameObject.FindGameObjectWithTag("Player1");
+        _rb = GetComponent<Rigidbody2D>();
 
-        if (player1.transform.localScale.x == 1)
+        if (_player1.transform.localScale.x == 1)
         {
-            rb.velocity = new Vector2(10, 0);
+            _rb.velocity = rightVelocity;
         }
-        else if(player1.transform.localScale.x == -1)
+        else if(_player1.transform.localScale.x == -1)
         {
+            //左向きの時向きを変える
             transform.localScale = new Vector3(-1, 1, 1);
-            rb.velocity = new Vector2(-10, 0);
+            _rb.velocity = leftVelocity;
         }
         
     }
@@ -28,7 +38,7 @@ public class ArrowGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //画面外の一定の距離を超えたら消す
         if (transform.position.x < -8 || transform.position.x > 2)
         {
             Destroy(gameObject);
@@ -41,7 +51,7 @@ public class ArrowGenerator : MonoBehaviour
 
         if (collision.gameObject.tag == "Player2")
         {
-            collision.gameObject.GetComponent<Player2controller>().Ondamage(10);
+            collision.gameObject.GetComponent<Player2controller>().Ondamage(damage);
             Destroy(gameObject);
             if (x > 0)
             {
